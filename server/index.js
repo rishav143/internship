5a8c177abf165776787a6a344114c547d988782e
Rishav Choudhary <rc8344353@gmail.com>
2024-07-22 18:45:10 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
-const express =require("express")
+require('dotenv').config();
+const express = require("express")
 const app = express()
-const cors = require("cors")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const router = require("./routes/user")
+const path = require('path');
 const cookieParser = require("cookie-parser")
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(cors({
-    origin:["http://localhost:3001"],
-    credentials:true
-}))
-mongoose.connect("mongodb://localhost:27017/internships").then(()=>{
-    console.log("connected to database")
-})
-app.use("/auth",router)
-app.listen(3000,()=>{
+
+//use the client app
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+//connecting to mongobd using Mongoose
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.use("/auth", router)
+app.listen(3000, () => {
     console.log("server is running on port 3000")
 })
 
